793c5bda2a54389072b8b07b91c3e3b1e17b7507
Ismael Ordás Carro <ismael.ordas@narrativa.com>
2022-12-09 20:04:09 +0100

    Save user

--- server/src/routers/users.js
+++ server/src/routers/users.js
@@ -4,16 +4,16 @@ const router = new express.Router();
 import {User} from '../models/users.js';
 
 router.post('/users', async (req, res) => {
-    console.log('test')
-  const user = new User(req.body);
-
   try {
-    console.log('try')
-    /* await user.save();
-    const token = await user.generateAuthToken(); */
-    res.status(201).send('test');
+    if (!Object.keys(req.body) || !req.body.username || !req.body.password) {
+      res.status(400).send(e);
+    }
+
+    const user = new User(req.body);
+    await user.save();
+    const token = await user.generateAuthToken();
+    res.status(201).send({user, token});
   } catch (e) {
-    console.log(e)
     res.status(400).send(e);
   }
 })
